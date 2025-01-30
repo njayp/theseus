d59ebadafc11c0ca619ab448d3bf566bc0f59198
nick powell <nickjaypowell@gmail.com>
2025-01-29 19:23:33 -0800

    organization

---
+++ pkg/manager/helpers.go
@@ -0,0 +1,48 @@
+package manager
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+)
+
+// createAndStartContainer creates and starts a new container for the given image
+func (m *Manager) createAndStartContainer(ctx context.Context, config Config) (string, error) {
+	resp, err := m.client.ContainerCreate(ctx, config.ContainerConfig, config.HostConfig, config.NetworkConfig, nil, "")
+	if err != nil {
+		return "", fmt.Errorf("failed to create container from image %s: %v", config.ContainerConfig.Image, err)
+	}
+
+	if err := m.client.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		return "", fmt.Errorf("failed to start container %s: %v", resp.ID, err)
+	}
+
+	return resp.ID, nil
+}
+
+// stopAndRemoveContainer removes a container by its ID
+func (m *Manager) stopAndRemoveContainer(ctx context.Context, id string) error {
+	err := m.client.ContainerStop(ctx, id, container.StopOptions{})
+	if err != nil {
+		return err
+	}
+
+	return m.client.ContainerRemove(ctx, id, container.RemoveOptions{})
+}
+
+func (m *Manager) pullImage(ctx context.Context, imageName string) error {
+	// Pull the latest version of the image
+	reader, err := m.client.ImagePull(ctx, imageName, image.PullOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to pull latest image %s: %v", imageName, err)
+	}
+	defer reader.Close()
+
+	// Print the pull output
+	_, err = io.Copy(os.Stdout, reader)
+	return err
+}
--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -3,12 +3,8 @@ package manager
 import (
 	"context"
 	"fmt"
-	"io"
-	"os"
 	"sync"
 
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 )
 
@@ -80,20 +76,6 @@ func (m *Manager) AddImage(ctx context.Context, config Config) error {
 	return nil
 }
 
-// createAndStartContainer creates and starts a new container for the given image
-func (m *Manager) createAndStartContainer(ctx context.Context, config Config) (string, error) {
-	resp, err := m.client.ContainerCreate(ctx, config.ContainerConfig, config.HostConfig, config.NetworkConfig, nil, "")
-	if err != nil {
-		return "", fmt.Errorf("failed to create container from image %s: %v", config.ContainerConfig.Image, err)
-	}
-
-	if err := m.client.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
-		return "", fmt.Errorf("failed to start container %s: %v", resp.ID, err)
-	}
-
-	return resp.ID, nil
-}
-
 // UpgradeImage pulls the latest version of the image and replaces the running container
 func (m *Manager) UpgradeImage(ctx context.Context, build BuildPayload) error {
 	m.Lock()
@@ -124,7 +106,7 @@ func (m *Manager) UpgradeImage(ctx context.Context, build BuildPayload) error {
 	// Update the stored image ID
 	ic.ImageId = digest.ID
 
-	err = m.removeContainer(ctx, ic.ContainerID)
+	err = m.stopAndRemoveContainer(ctx, ic.ContainerID)
 	if err != nil {
 		return fmt.Errorf("failed to remove old container %s: %v", ic.ImageName, err)
 	}
@@ -150,33 +132,10 @@ func (m *Manager) RemoveImage(ctx context.Context, imageName string) error {
 		return fmt.Errorf("image %s is not being managed", imageName)
 	}
 
-	if err := m.removeContainer(ctx, ic.ContainerID); err != nil {
+	if err := m.stopAndRemoveContainer(ctx, ic.ContainerID); err != nil {
 		return fmt.Errorf("failed to remove old container %s: %v", ic.ImageName, err)
 	}
 
 	delete(m.images, imageName)
 	return nil
-}
-
-// removeContainer removes a container by its ID
-func (m *Manager) removeContainer(ctx context.Context, id string) error {
-	err := m.client.ContainerStop(ctx, id, container.StopOptions{})
-	if err != nil {
-		return err
-	}
-
-	return m.client.ContainerRemove(ctx, id, container.RemoveOptions{})
-}
-
-func (m *Manager) pullImage(ctx context.Context, imageName string) error {
-	// Pull the latest version of the image
-	reader, err := m.client.ImagePull(ctx, imageName, image.PullOptions{})
-	if err != nil {
-		return fmt.Errorf("failed to pull latest image %s: %v", imageName, err)
-	}
-	defer reader.Close()
-
-	// Print the pull output
-	_, err = io.Copy(os.Stdout, reader)
-	return err
 }
